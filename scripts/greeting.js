f622cc18ad18ff2d762d55e6fa946f905dc8b9f9
lopezju86 <54110007+lopezju86@users.noreply.github.com>
2019-08-27 15:29:40 -0400

    bug fix

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".js-form");
-const input = form.querySelector("inpput");
+const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
